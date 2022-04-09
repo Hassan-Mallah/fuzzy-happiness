4a21337d3496e77a7e5d9c5a0d12fa985465d9b3
Hassan <hassansmallah@gmail.com>
2022-04-09 10:25:10 +0300

    DataFrame

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-import pandas
+import pandas as pd
 
-print(pandas.__version__)
+print('version:', pd.__version__, ']n')
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
+
+print('this is a data frame:\n', df)
