cf641fcdc1c5fbd49cc2a2c072a1a0fb9de28e45
Hassan <hassansmallah@gmail.com>
2022-04-24 10:57:22 +0300

    shape

--- subset.py
+++ subset.py
@@ -1,9 +1,19 @@
 import pandas as pd
-from pandas import DataFrame
+from pandas import Series
+
+
+def print_head(series: Series):
+    print(series.head())
+
+
+#  A pandas Series is 1-dimensional and only the number of rows is returned.
+def print_shape(series: Series):
+    print(ages.shape)
+
 
 titanic = pd.read_csv("data/titanic.csv")
 
 # I’m interested in the age of the Titanic passengers.
 ages = titanic["Age"]
 
-print(ages.head())
+print_shape(ages)
